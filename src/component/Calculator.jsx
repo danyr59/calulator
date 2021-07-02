b0f22032d44490d5f9f2ce5607662f4ca61125ef
danyr59 <danyelrange59@gmail.com>
2021-07-02 12:36:50 -0400

    add style at display

--- src/component/Calculator.jsx
+++ src/component/Calculator.jsx
@@ -1,25 +1,30 @@
 import "../style/Calculator.scss";
 import bottoms from "../services/bottoms.json";
-function Calculator() {
-  let bottom = bottoms.bottom.map((current,i) => {
-    console.log(typeof parseInt(current.style));
-    return ( 
-      <buttom 
-        translate="no" 
-        value={current.value} 
+import Display from "./Display.jsx";
+function Calculator(props) {
+  let bottom = bottoms.bottom.map((current, i) => {
+    return (
+      <buttom
+        translate="no"
+        value={current.value}
         id={current.id}
         className={`colors-${i}`}
-        style={current.value == "equal" ? {gridColumn: current.style, gridRow: "4/6"} : {gridColumn: current.style} }
+        style={
+          current.value == "equal"
+            ? { gridColumn: current.style, gridRow: "4/6" }
+            : { gridColumn: current.style }
+        }
       >
         {current.keyTrigger}
       </buttom>
-    )
+    );
   });
   return (
-    <div className="calculator-container">
-        {bottom}
+    <div className="calculator"  style={{ position: "relative" }}>
+      <Display />
+      <div className="calculator-container">{bottom}</div>
     </div>
-    );
+  );
 }
 
 export default Calculator;
---
+++ src/component/Display.jsx
@@ -0,0 +1,12 @@
+import '../style/Display.scss'
+
+const Display = (props) => {
+  return (
+    <div className="display"> 
+      <div className="formulaScreen"></div>
+      <div className="outputScreen"></div>
+    </div>
+  );
+
+}
+export default Display;
